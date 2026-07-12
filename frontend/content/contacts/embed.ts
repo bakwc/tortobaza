cf324825cddd1fe4931a52fc345360884ad43628
Filipp Ozinov <filipp@bakanov.su>
2026-07-12 12:48:36 +0400

    Updated map

--- frontend/content/contacts/embed.ts
+++ frontend/content/contacts/embed.ts
@@ -1,9 +1,3 @@
-export const MAP_COORDS = "41.621184,41.614267";
+export const MAP_LINK = "https://maps.app.goo.gl/ayq8YRmFDiBu8Wi29";
 
-export const MAP_LINK = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
-  MAP_COORDS,
-)}`;
-
-export const MAP_EMBED = `https://maps.google.com/maps?q=${encodeURIComponent(
-  MAP_COORDS,
-)}&hl=en&z=15&output=embed`;
+export const MAP_EMBED = `https://maps.google.com/maps?q=0x406785b1fa479067:0xfb92a86e78af2a31&hl=en&z=15&output=embed`;
