af8124092f1f44d8628c0df7c960ee5bedbb30a2
Filipp Ozinov <filipp@bakanov.su>
2026-08-26 01:36:04 +0400

    Try fix map

--- frontend/content/contacts/embed.ts
+++ frontend/content/contacts/embed.ts
@@ -1,3 +1,4 @@
-export const MAP_LINK = "https://maps.app.goo.gl/ayq8YRmFDiBu8Wi29";
+export const MAP_LINK = "https://maps.app.goo.gl/YEbxL6z567SGWkmo8";
 
-export const MAP_EMBED = `https://maps.google.com/maps?q=0x406785b1fa479067:0xfb92a86e78af2a31&hl=en&z=15&output=embed`;
+export const MAP_EMBED =
+  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1500!2d41.6142691!3d41.6211876!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x406785b1fa479067%3A0xfb92a86e78af2a31!2sSweet%26Chill!5e0!3m2!1sen!2sge";
